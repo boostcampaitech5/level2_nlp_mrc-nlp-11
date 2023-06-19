3c1d0a277875d2ee3b2bbdaf1be9b726f54ec660
jeongmin <sssi6792@gmail.com>
2023-06-19 09:04:44 +0000

    feat: add train test shell script

---
+++ code/inference.sh
@@ -0,0 +1,6 @@
+python inference.py \
+--output_dir ./models/test_dataset \
+--dataset_name ../data/test_dataset/\
+--model_name_or_path ./models/train_dataset/ \
+--do_predict \
+--overwrite_output_dir
